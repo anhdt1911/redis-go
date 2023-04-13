996f841aeb51af310ba5829b95eb1bfdd7e9a8b5
Anh Duong Tien <anhdt1911.work@gmail.com>
2023-04-13 15:44:32 +0700

    response to ping

--- app/server.go
+++ app/server.go
@@ -13,7 +13,7 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:6380")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6380")
 		fmt.Println(err)
